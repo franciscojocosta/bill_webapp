29bd448d26468acd43732473ef7c7e54ef3e3dac
Francisco Costa <franciscojocosta@gmail.com>
2022-11-14 20:27:01 +0000

    Updated table

--- bill_webapp/bill_webapp/settings.py
+++ bill_webapp/bill_webapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_tables2'
 ]
 
 MIDDLEWARE = [
@@ -125,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[
-    'bill_webapp\static'
+    os.path.join(BASE_DIR, "static/")
 ]
 
 # Default primary key field type
